dfd74e06b8004079a13392cdd39bf27cad51ad65
Matt Crandell <mrcrandell@gmail.com>
2016-04-02 18:25:42 -0400

    Added theme to git.

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+	return gulp.src('library/sass/style.scss')
+        .pipe(sourcemaps.init())
+         .pipe(sass({
+             outputStyle: 'compressed',
+             includePaths: [
+                 'node_modules/bootstrap-sass/assets/stylesheets/',
+                'node_modules/font-awesome/scss/'
+             ]
+         }) .on('error', sass.logError))
+        .pipe(sourcemaps.write())
+         .pipe(gulp.dest('library/css/')); 
+});
+
+gulp.task('js', function(){
+    return gulp.src(['library/js/jquery-1.12.2.js', 'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('library/js'));
+});
+
+// Copy Font Awesome Fonts
+gulp.task('copy-font-awesome', function () {
+    return gulp
+        .src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('library/fonts'));
+});
+
+// Copy Bootstrap Fonts
+gulp.task('copy-bootstrap', function () {
+    return gulp
+        .src('node_modules/bootstrap-sass/assets/fonts/bootstrap/*')
+        .pipe(gulp.dest('library/fonts/bootstrap'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('library/sass/*.scss', ['sass']);
+    gulp.watch('library/js/*.js', ['js']);
+});
+
+gulp.task('default', ['sass', 'js', 'copy-font-awesome', 'copy-bootstrap'], function(){});
