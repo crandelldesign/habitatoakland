0ebd647153fc5b733bf3e22e74061ba2c77a0be8
Matt Crandell <mrcrandell@gmail.com>
2016-10-06 06:21:04 -0400

    Updated restore template. Updated styling.

--- page-restore.php
+++ page-restore.php
@@ -69,30 +69,6 @@
 
 			<?php endif; ?>
 
-			<?php
-				$womenbuildIdObj = get_category_by_slug('women-build');
-				$womenbuildId = $womenbuildIdObj->term_id;
-				$eventsIdObj = get_category_by_slug('event');
-				$eventsId = $eventsIdObj->term_id;
-			?>
-
-			<!-- Get Events -->
-			<?php query_posts( array( 'category__and' => array( $womenbuildId, $eventsId ) ) ); ?>
-    		<div class="row margin-bottom-10">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) { ?>
-				<div class="col-sm-6 col-md-4 margin-bottom-15 text-center">
-					<a class="btn-img btn btn-block" href="<?php echo get_permalink(); ?>" target="_blank">
-						<span class="title"><?php the_title() ;?></span>
-						<div class="img-holder" style="background-image:url(<?php echo the_post_thumbnail_url('full') ?>)"></div>
-						<span class="learn-more">See More <i class="fa fa-angle-right"></i></span>
-					</a>
-			   	</div>
-			   	<?php } ?>
-			<?php endwhile; endif; ?>
-			<?php wp_reset_query(); ?>
-			</div>
-
 		</div>
 	</div>
 </div>
