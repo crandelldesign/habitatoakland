cdb6d612c33f40a898ea7e991be3edaa354a212e
Matt Crandell <mrcrandell@gmail.com>
2019-08-21 23:10:44 -0400

    Updated donation form.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,25 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="icon" type="image/x-icon" href="<?= \App\asset_path('images/favicon.ico') ?>" />
+  <link rel="apple-touch-icon" sizes="57x57" href="<?= \App\asset_path('images/apple-icon-57x57.png') ?>">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?= \App\asset_path('images/apple-icon-60x60.png') ?>">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?= \App\asset_path('images/apple-icon-72x72.png') ?>">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= \App\asset_path('images/apple-icon-76x76.png') ?>">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?= \App\asset_path('images/apple-icon-114x114.png') ?>">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?= \App\asset_path('images/apple-icon-120x120.png') ?>">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?= \App\asset_path('images/apple-icon-144x144.png') ?>">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?= \App\asset_path('images/apple-icon-152x152.png') ?>">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= \App\asset_path('images/apple-icon-180x180.png') ?>">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?= \App\asset_path('images/android-icon-192x192.png') ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= \App\asset_path('images/favicon-32x32.png') ?>">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?= \App\asset_path('images/favicon-96x96.png') ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= \App\asset_path('images/favicon-16x16.png') ?>">
+  <link rel="manifest" href="<?= \App\asset_path('images/manifest.json') ?>">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="<?= \App\asset_path('images/ms-icon-144x144.png') ?>">
+  <meta name="theme-color" content="#888B8D">
+
   @php wp_head() @endphp
 </head>
