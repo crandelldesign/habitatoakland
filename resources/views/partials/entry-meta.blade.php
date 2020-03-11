064702ad915cb1d62c978f9b75fb1ca487813764
Matt Crandell <mrcrandell@gmail.com>
2020-03-10 23:09:18 -0400

    Updated meta on event posts.

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,11 @@
 <span class="post-date">
   <i class="fa fa-calendar fa-fw" aria-hidden="true"></i>&nbsp;
-  <?php printf( '<time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> ', get_the_time('Y-m-j', get_the_ID()), get_the_time(get_option('date_format'), get_the_ID())) ?>
+  <?php // If an event
+  if (get_post_type() == 'tribe_events'): ?>
+    <?= (tribe_event_is_all_day())?tribe_get_start_date(null, true, 'l, F jS, Y'):tribe_get_start_date(null, true, 'l, F jS, Y g:i a') ?>
+  <?php else: // If not an event ?>
+    <?php printf( '<time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> ', get_the_time('Y-m-j', get_the_ID()), get_the_time(get_option('date_format'), get_the_ID())) ?>
+  <?php endif; ?>
 </span>
 <span class="categories">
 <?php $categories = get_the_category(get_the_ID()) ?>
