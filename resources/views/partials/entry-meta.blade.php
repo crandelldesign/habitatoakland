2182795e0d871fd2dcf93d590a0f8dd5870b2ba0
Matt Crandell <mrcrandell@gmail.com>
2020-03-20 23:18:41 -0400

    Updated event handling and date formatting.

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,9 +1,10 @@
 <span class="post-date">
-  <i class="fa fa-calendar fa-fw" aria-hidden="true"></i>&nbsp;
   <?php // If an event
   if (get_post_type() == 'tribe_events'): ?>
-    <?= (tribe_event_is_all_day())?tribe_get_start_date(null, true, 'l, F jS, Y'):tribe_get_start_date(null, true, 'l, F jS, Y g:i a') ?>
+    <i class="fa fa-calendar-check-o" aria-hidden="true"></i>&nbsp;
+    <?= (tribe_event_is_all_day())?tribe_get_start_date(null, true, 'F jS, Y'):tribe_get_start_date(null, true, 'l, F jS, Y g:i a') ?>
   <?php else: // If not an event ?>
+    <i class="fa fa-calendar fa-fw" aria-hidden="true"></i>&nbsp;
     <?php printf( '<time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> ', get_the_time('Y-m-j', get_the_ID()), get_the_time(get_option('date_format'), get_the_ID())) ?>
   <?php endif; ?>
 </span>
